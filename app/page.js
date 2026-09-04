2b4fa22acd64ab2ac28a49b9e09340718653c56e
jaishika-times <jaishika@times.my>
2026-09-04 11:57:33 +0530

    Fix syntax error in productivity section brackets

--- app/page.js
+++ app/page.js
@@ -325,7 +325,6 @@ export default function DashboardPage() {
                       </table>
                     </div>
                   )}
-                </>
                     </>
                   )}
                 </>
